829d40ebb7c481bc4dac605fad550e3de49459bd
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-07-22 13:05:05 -0300

    first commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Slot } from 'expo-router';
+import 'react-native-reanimated';
+import '../../global.css';
+
+export default function RootLayout() {
+  return (
+    <>
+      <Slot/>
+    </>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index(){
+  return(
+    <View className="flex items-center justify-center h-full">
+      <Text>Teste</Text>
+    </View>
+  )  
+}
