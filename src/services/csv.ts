db920813ff3691e2b8ec0421f579a744f8aba61b
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-07-23 13:05:04 -0300

    feat: add new connection with backend

--- src/services/csv.ts
+++ src/services/csv.ts
@@ -4,3 +4,8 @@ export async function getListCsv(){
   const response = await api.get('/csv-files');
   return response.data;
 }
+
+export async function selectCsv(filename: string){
+  const response = await api.post('/select-csv', { filename });
+  return response.data;
+}
