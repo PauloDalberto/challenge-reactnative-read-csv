2aab654e6d5d4a68917178af8744e1a4d634fdd8
Paulo Henrique Dalberto <phdalberto2016@gmail.com>
2025-07-24 17:19:23 -0300

    fix: fix css to show everything in the middle of the screen

--- src/components/loading/loading.tsx
+++ src/components/loading/loading.tsx
@@ -2,7 +2,7 @@ import { View, Text, ActivityIndicator } from 'react-native'
 
 export default function Loading() {
   return (
-    <View className="flex-1 justify-center items-center bg-white px-4">
+    <View className="flex h-full justify-center items-center bg-white px-4">
       <ActivityIndicator size="large" color="#6366F1" /> 
       <Text className="mt-4 text-lg text-gray-700 text-center font-medium">
         O conteúdo está sendo carregado, por favor aguarde...
